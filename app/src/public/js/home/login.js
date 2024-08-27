8293f25e73c257e93bb358c7bf8e71464d60f8b3
seunghoo090004 <151715733+seunghoo090004@users.noreply.github.com>
2024-08-27 16:02:54 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict"
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+//두 번째로 넘어오는 파라미터는 함수이기에 function을 만들어줘야함
+loginBtn.addEventListener("click", login);
+
+// 로그인이 눌릴 때마다 위에 있는 id와 psword 값을 잘 가져와줘야함.
+function login(){
+    const req = {
+        id: id.value, //id, psword안에 있는 박스 값은 id.value로 가져옴
+        psword: psword.value, 
+    };
+    //위에 해당하는 값을 서버에 전달 -> fetch();
+    console.log(req);
+}

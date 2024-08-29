0384d707ff2fba705e8314866d6395f79b489bde
seunghoo090004 <151715733+seunghoo090004@users.noreply.github.com>
2024-08-29 17:20:56 +0900

    서버의 응답데이터 처리 in 프런트

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -34,7 +34,7 @@ const process = {
         
         return res.json({
             success: false,
-            msg: "로그인에 실패하셨습니다.",
+            msg: "로그인에 실패하셨습니다.", //키
         });
     },
 };

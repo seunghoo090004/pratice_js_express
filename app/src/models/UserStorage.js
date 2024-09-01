50c6add5fbdd9a384445368b758b0515f56b8585
seunghoo090004 <151715733+seunghoo090004@users.noreply.github.com>
2024-09-01 15:41:47 +0900

    user 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// 유저 정보
 class UserStorage{
 // static을 사용하여 정적 변수로 만들어서 클래스 자체에서 users 변수에 접근 가능
     static #users = {
@@ -17,6 +18,19 @@ class UserStorage{
             return newUsers;
         }, {});
         return newUsers;
+    }
+
+    //회원 정보 순회하며 인증 로직
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users); // => [id, psword, name]
+        const userInfo = userKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
     }
 }
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const  User = require("../../models/User");
 
 const output = {
     home: (req,res) => {
@@ -9,41 +9,16 @@ const output = {
     login: (req,res) => {
         res.render("home/login");
     },
-}
+};
 
-
-
+//회원 login 로직 순회 후 답변 프로세스
 const process = {
-    login: (req,res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id", "psword");
-
-        const response ={};
-//프론트에서 전달한 id가 users에 있으면 
-        if(users.id.includes(id)){
-//아이디의 인덱스를 가져온다.
-            const idx = users.id.indexOf(id);
-//if문을 하나 더 써서 psword가 같다면 json으로 리턴을 해준다
-            if(users.psword[idx] === psword){
-                response.success = true;
-                return res.json(response);//sucess라는 오브젝트를 true로 만들어서 json형식으로 res(응답)을 해준다
-            }
-        }
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
+    login: (req, res) => {
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },
 };
-//5분!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// const hello = (req, res) => {
-//     res.render("home/index");
-// };
-
-// const login = (req,res) =>{
-//     res.render("home/login");
-// };
 
 module.exports = {
     output,
